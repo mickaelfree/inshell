dc07a1cfbc84ba4f36e077d0bd3e6899126c2e33
mickmart <mickmart@student.42lyon.fr>
2025-05-26 21:27:11 +0200

    unfixed unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,28 +6,12 @@
 /*   By: mickmart <mickmart@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:36:20 by mickmart          #+#    #+#             */
-/*   Updated: 2025/05/26 19:11:20 by mickmart         ###   ########.fr       */
+/*   Updated: 2025/05/26 21:13:59 by mickmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 //TODO::
 #include "../../includes/inshell.h"
 
-int is_valid_identifier(char *str)
-{
-        int i;
-
-        i = 0;
-        if (!ft_isalpha(str[i]) && str[i] != '_')
-                return (0);
-        i++;
-        while (str[i])
-        {
-                if (!ft_isalnum(str[i]) && str[i] != '_')
-                        return (0);
-                i++;
-        }
-        return (1);
-}
 
 int builtin_unset(char **args, char **envp)
 {
@@ -35,17 +19,6 @@ int builtin_unset(char **args, char **envp)
         i = 1;
         if (!args[1])
                 return (1);
-        while (args[i])
-        {
-                if (!is_valid_identifier(args[i]))
-                {
-                        printf("unset: %s: not a valid identifier\n", args[i]);
-                        return (1);
-                }
-                i++;
-        }
-        printf("unset: %s:valid identifier\n", args[i]);
-        i = 0;
         while (envp[i])
         {
                 if (!strcmp(envp[i], args[1]))
