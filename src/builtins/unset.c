26ac27038fc418178c8e384821a321942d0d6102
mickmart <mickmart@student.42lyon.fr>
2025-05-27 15:41:11 +0200

    add while in unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,28 +6,37 @@
 /*   By: mickmart <mickmart@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:36:20 by mickmart          #+#    #+#             */
-/*   Updated: 2025/05/26 21:13:59 by mickmart         ###   ########.fr       */
+/*   Updated: 2025/05/27 15:36:37 by mickmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-//TODO::
+// TODO::
 #include "../../includes/inshell.h"
 
-
-int builtin_unset(char **args, char **envp)
+int	builtin_unset(char **args, char **envp)
 {
-        int i;
-        i = 1;
-        if (!args[1])
-                return (1);
-        while (envp[i])
-        {
-                if (!strcmp(envp[i], args[1]))
-                {
-                        free(envp[i]);
-                        envp[i] = envp[i + 1];
-                        i++;
-                }
-                i++;
-        }
-        return (0);
+	int	i;
+
+	if (!args[1])
+		return (1);
+	args++;
+	while (*args)
+	{
+		i = 1;
+		while (envp[i])
+		{
+			if (!strcmp(envp[i], args[1]))
+			{
+				free(envp[i]);
+				while (envp[i + 1])
+				{
+					envp[i] = envp[i + 1];
+					i++;
+				}
+				envp[i] = NULL;
+			}
+			i++;
+		}
+		args++;
+	}
+	return (0);
 }
