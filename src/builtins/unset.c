e7965f8d1e2b1d5207620c314f47d6c76be7b647
mickmart <mickmart@student.42lyon.fr>
2025-06-02 14:43:18 +0200

    made a copy of env for cmd

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: mickmart <mickmart@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:36:20 by mickmart          #+#    #+#             */
-/*   Updated: 2025/05/27 16:03:19 by mickmart         ###   ########.fr       */
+/*   Updated: 2025/05/27 16:01:35 by mickmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 // TODO::
@@ -15,26 +15,29 @@
 int	builtin_unset(char **args, char **envp)
 {
 	int	i;
+	int	j;
 
 	if (!args[1])
 		return (1);
 	args++;
 	while (*args)
 	{
-		i = 1;
+		i = 0;
 		while (envp[i])
 		{
 			if (!strcmp(envp[i], *args))
 			{
 				free(envp[i]);
-				while (envp[i + 1])
+				j = i;
+				while (envp[j + 1])
 				{
-					envp[i] = envp[i + 1];
-					i++;
+					envp[j] = envp[j + 1];
+					j++;
 				}
-				envp[i] = NULL;
+				envp[j] = NULL;
 			}
-			i++;
+			else
+				i++;
 		}
 		args++;
 	}
