9fcd7bf2837971a6c33cb431b12a1e34dd4c1b75
mickmart <mickaelfree1martins@gmail.com>
2025-09-19 20:58:31 +0200

    norme unset

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,11 +6,40 @@
 /*   By: zsonie <zsonie@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:35:41 by mickmart          #+#    #+#             */
-/*   Updated: 2025/09/06 20:42:42 by zsonie           ###   ########lyon.fr   */
+/*   Updated: 2025/09/19 18:12:16 by mickmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mandatoshell.h"
+
+char	**init_env(char ***envp)
+{
+	int		count;
+	char	**new_env;
+	char	**cur_env;
+	int		i;
+
+	cur_env = *envp;
+	if (!cur_env)
+		return (NULL);
+	count = ft_count((void**)cur_env);
+	new_env = malloc(sizeof(char *) * (count + 1));
+	if (!new_env)
+		return (NULL);
+	i = 0;
+	while (i < count)
+	{
+		new_env[i] = ft_strdup((*envp)[i]);
+		if (!new_env[i])
+		{
+                        ft_free_tab((void**)new_env);
+			return (NULL);
+		}
+		i++;
+	}
+	new_env[count] = NULL;
+	return (new_env);
+}
 
 int	builtin_env(char **args, char ***envp)
 {
@@ -30,35 +59,3 @@ int	builtin_env(char **args, char ***envp)
 	return (0);
 }
 
-char	**init_env(char ***envp)
-{
-	int		count;
-	char	**new_env;
-	char	**cur_env;
-	int		i;
-
-	cur_env = *envp;
-	if (!cur_env)
-		return (NULL);
-	count = 0;
-	while (cur_env[count])
-		count++;
-	new_env = malloc(sizeof(char *) * (count + 1));
-	if (!new_env)
-		return (NULL);
-	i = 0;
-	while (i < count)
-	{
-		new_env[i] = ft_strdup((*envp)[i]);
-		if (!new_env[i])
-		{
-			while (--i >= 0)
-				free(new_env[i]);
-			free(new_env);
-			return (NULL);
-		}
-		i++;
-	}
-	new_env[count] = NULL;
-	return (new_env);
-}
